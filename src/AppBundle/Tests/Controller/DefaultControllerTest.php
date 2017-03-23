a931f116a69b1c92295e791a879da4496fd44329
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2017-03-23 23:37:45 +0100

    added test redirect when not set level

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -14,4 +14,13 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    public function testNotSetLevel()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/game');
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
 }
